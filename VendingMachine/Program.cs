c4bd4e0e1a9c5c807d457998ff5273f73427bde1
Po-CHEN LEE <asd06a@yahoo.com.tw>
2021-03-11 23:04:47 +0800

    設置和硬幣相關動作

--- VendingMachine/Program.cs
+++ VendingMachine/Program.cs
@@ -4,9 +4,68 @@ namespace VendingMachine
 {
     class Program
     {
+        private static decimal _totalAmount = 0;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool isContinue = true;
+            Console.WriteLine("Hello Vending machine !");
+            while (isContinue)
+            {
+                ShowMoney();
+                ShowMainMenu();
+                var userInput = Console.ReadLine();
+                switch (userInput.ToUpper())
+                {
+                    case "A":
+                    case "COIN":
+                        Console.WriteLine("ADD 10 Coint To vending machine");
+                        _totalAmount += 10;
+                        break;
+                    case "B":
+                    case "BUY":
+                        Console.WriteLine("Selected BUY");
+                        break;
+                    case "C":
+                    case "REFUND":
+                        RefundMoney();
+                        break;
+                    case "D":
+                    case "SETPRODUCT":
+                        Console.WriteLine("Selected SETPRODUCT");
+                        break;
+                    case "E":
+                    case "LEAVE":
+                        if (_totalAmount != 0)
+                        {
+                            RefundMoney();
+                            ShowMoney();
+                        }
+
+                        Console.WriteLine("LEAVE vending machine");
+                        isContinue = false;
+                        break;
+                }
+
+                Console.WriteLine("\r\n===========");
+            }
+        }
+
+        static void ShowMoney()
+        {
+            Console.WriteLine($"Now Total : ${_totalAmount}");
+        }
+
+        static void ShowMainMenu()
+        {
+            Console.WriteLine("Options: (A)Coin, (B)Buy, (C)Refund, (D)SetProduct, (E)Leave");
+        }
+
+        static void RefundMoney()
+        {
+            Console.WriteLine($"Refund Money : ${_totalAmount}");
+            _totalAmount = 0;
+
         }
     }
 }
