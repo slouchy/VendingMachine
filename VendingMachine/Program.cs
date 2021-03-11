890a063ca4ed3df118956158c76580271ae4885d
Po-CHEN LEE <asd06a@yahoo.com.tw>
2021-03-11 23:30:00 +0800

    購買物品

--- VendingMachine/Program.cs
+++ VendingMachine/Program.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace VendingMachine
 {
     class Program
     {
         private static decimal _totalAmount = 0;
+        private static Dictionary<int, decimal> _productPrices = new Dictionary<int, decimal>()
+        {
+            [1] = 50,
+            [2] = 30,
+            [3] = 60,
+            [4] = 10,
+            [5] = 20,
+        };
+        private static decimal _productPrice = 50;
+
 
         static void Main(string[] args)
         {
@@ -24,7 +36,15 @@ namespace VendingMachine
                         break;
                     case "B":
                     case "BUY":
-                        Console.WriteLine("Selected BUY");
+                        var minProdcutPrice = _productPrices.Min(x => x.Value);
+                        if (_totalAmount < minProdcutPrice)
+                        {
+                            Console.WriteLine($"Min product price is ${minProdcutPrice}, but there is only total: ${_totalAmount}");
+                        }
+                        else
+                        {
+                            ChooseProduct();
+                        }
                         break;
                     case "C":
                     case "REFUND":
@@ -66,6 +86,32 @@ namespace VendingMachine
             Console.WriteLine($"Refund Money : ${_totalAmount}");
             _totalAmount = 0;
 
+        }
+
+        static void ChooseProduct()
+        {
+            IEnumerable<KeyValuePair<int, decimal>> filterProducts = _productPrices.Where(x => x.Value <= _totalAmount);
+            var productOption = filterProducts.Select(x => $"Product ({x.Key}) Price: {x.Value}");
+            Console.WriteLine($"Can Buy : {string.Join(" , ", productOption)}. Or Choose (A)Abandon choose product");
+            Console.WriteLine($"Please enten number: ");
+            var userInput = Console.ReadLine();
+
+            var selectedProduct = filterProducts.FirstOrDefault(x => userInput == x.Key.ToString());
+
+            if (userInput.Equals("A", StringComparison.OrdinalIgnoreCase) ||
+                userInput.Equals("Abandon", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Leave choose product");
+            }
+            else if (selectedProduct.Key == 0)
+            {
+                Console.WriteLine("Can't find product");
+            }
+            else
+            {
+                Console.WriteLine($"Got Product ({selectedProduct.Key})");
+                _totalAmount -= selectedProduct.Value;
+            }
         }
     }
 }
