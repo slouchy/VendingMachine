28711d7049565a025d697c58915ad25e38ab5576
Po-CHEN LEE <asd06a@yahoo.com.tw>
2021-03-13 23:43:41 +0800

    調整流程

--- VendingMachine/Program.cs
+++ VendingMachine/Program.cs
@@ -7,80 +7,156 @@ namespace VendingMachine
     class Program
     {
         private static decimal _totalAmount = 0;
-        private static List<MyProduct> _products = new List<MyProduct>()
+
+        private static List<ShelfModel> _shelfs = new List<ShelfModel>
         {
-            new MyProduct
+            new ShelfModel
             {
-                Location = 1,
-                Key = 1,
-                Price = 50
+                Idx = 0,
+                Required = new List<MixtureModel>
+                {
+                    new MixtureModel
+                    {
+                        Idx=0,
+                        Name = Mixture.Hot
+                    }
+                },
+                Optional = new List<MixtureModel>
+                {
+                    new MixtureModel
+                    {
+                        Idx=0,
+                        Name = Mixture.Lemon
+                    },
+                    new MixtureModel
+                    {
+                        Idx=1,
+                        Name = Mixture.Milk
+                    },
+                }
             },
-            new MyProduct
+            new ShelfModel
             {
-                Location = 1,
-                Key = 2,
-                Price = 30
+                Idx = 1,
+                Required = new List<MixtureModel>{
+                    new MixtureModel
+                    {
+                       Idx=0,
+                       Name=Mixture.Cold,
+                    },
+                    new MixtureModel
+                    {
+                       Idx=1,
+                       Name=Mixture.Ice,
+                    },
+                    new MixtureModel
+                    {
+                        Idx=2,
+                        Name = Mixture.Lemon
+                    }
+                },
             },
-            new MyProduct
+            new ShelfModel
             {
-                Location = 1,
-                Key = 3,
-                Price = 60
-            },
-            new MyProduct
-            {
-                Location = 1,
-                Key = 4,
-                Price = 10
-            },new MyProduct
-            {
-                Location = 1,
-                Key = 5,
-                Price = 20
-            },
-            new MyProduct
-            {
-                Location = 1,
-                Key = 6,
-                Price = 40
-            },
-            new MyProduct
-            {
-                Location = 2,
-                Key = 7,
-                Price = 80
-            },
-            new MyProduct
-            {
-                Location = 2,
-                Key = 8,
-                Price = 10
-            },new MyProduct
-            {
-                Location = 2,
-                Key = 9,
-                Price = 10
-            },
-            new MyProduct
-            {
-                Location = 2,
-                Key = 10,
-                Price = 20
-            },
-            new MyProduct
-            {
-                Location = 3,
-                Key = 11,
-                Price = 10
-            },
-            new MyProduct
-            {
-                Location = 3,
-                Key = 12,
-                Price = 60
-            },
+                Idx = 2,
+                Required = new List<MixtureModel>{
+                    new MixtureModel
+                    {
+                       Idx=0,
+                       Name=Mixture.Hot,
+                    },
+                    new MixtureModel
+                    {
+                        Idx=2,
+                        Name = Mixture.Milk
+                    }
+                },
+                Optional = new List<MixtureModel>
+                {
+                    new MixtureModel
+                    {
+                       Idx=1,
+                       Name=Mixture.Ice,
+                    },
+                }
+            }
         };
 
+        private static List<ProductModel> _products = new List<ProductModel>
+        {
+            new ProductModel
+            {
+                ShelfIdx = 0,
+                LocationIdx = 1,
+                Price = 10,
+            },
+            new ProductModel
+            {
+                ShelfIdx = 0,
+                LocationIdx = 2,
+                Price = 20,
+            },
+            new ProductModel
+            {
+                ShelfIdx = 0,
+                LocationIdx = 3,
+                Price = 30,
+            },
+            new ProductModel
+            {
+                ShelfIdx = 0,
+                LocationIdx = 4,
+                Price = 40,
+            },
+             new ProductModel
+            {
+                ShelfIdx = 0,
+                LocationIdx = 5,
+                Price = 50,
+            },
+            new ProductModel
+            {
+                ShelfIdx = 0,
+                LocationIdx = 6,
+                Price = 60,
+            },
+               new ProductModel
+            {
+                ShelfIdx = 1,
+                LocationIdx = 1,
+                Price = 10,
+            },
+            new ProductModel
+            {
+                ShelfIdx = 1,
+                LocationIdx = 2,
+                Price = 20,
+            },
+            new ProductModel
+            {
+                ShelfIdx = 1,
+                LocationIdx = 3,
+                Price = 30,
+            },
+            new ProductModel
+            {
+                ShelfIdx = 1,
+                LocationIdx = 4,
+                Price = 40,
+            },
+            new ProductModel
+            {
+                ShelfIdx = 2,
+                LocationIdx = 3,
+                Price = 30,
+            },
+            new ProductModel
+            {
+                ShelfIdx = 2,
+                LocationIdx = 4,
+                Price = 40,
+            },
+        };
 
 
         static void Main(string[] args)
@@ -91,51 +167,43 @@ namespace VendingMachine
             {
                 ShowMoney();
                 ShowMainMenu();
-                var userInput = Console.ReadLine();
-                switch (userInput.ToUpper())
-                {
-                    case "A":
-                    case "COIN":
-                        Console.WriteLine("ADD 10 Coint To vending machine");
-                        _totalAmount += 10;
-                        break;
-                    case "B":
-                    case "BUY":
-                        Console.WriteLine("Choose Product page (1/2)");
-                        userInput = Console.ReadLine();
-                        if (int.TryParse(userInput, out var tmp) && (tmp > 0 && tmp < 3))
-                        {
-                            ChooseProduct(tmp);
-                        }
-                        else
-                        {
-                            Console.WriteLine("product page error");
-                        }
-
-                        break;
-                    case "C":
-                    case "REFUND":
-                        RefundMoney();
-                        break;
-                    case "D":
-                    case "SETPRODUCT":
-                        Console.WriteLine("Selected SETPRODUCT");
-                        break;
-                    case "E":
-                    case "LEAVE":
-                        if (_totalAmount != 0)
-                        {
-                            RefundMoney();
-                            ShowMoney();
-                        }
-
-                        Console.WriteLine("LEAVE vending machine");
-                        isContinue = false;
-                        break;
-                }
+                isContinue = ChooseMainOptions();
 
                 Console.WriteLine("\r\n===========");
             }
+        }
+
+        static bool ChooseMainOptions()
+        {
+            var isContinue = true;
+            var userInput = Console.ReadLine();
+            switch (userInput.ToUpper())
+            {
+                case "A":
+                case "COIN":
+                    Console.WriteLine("ADD 10 Coint To vending machine");
+                    _totalAmount += 10;
+                    break;
+                case "B":
+                case "BUY":
+                    BuyProduct();
+                    break;
+                case "C":
+                case "REFUND":
+                    RefundMoney();
+                    break;
+                case "D":
+                case "SETPRODUCT":
+                    Console.WriteLine("Selected SETPRODUCT");
+                    break;
+                case "E":
+                case "LEAVE":
+                    Console.WriteLine("LEAVE vending machine");
+                    isContinue = false;
+                    break;
+            }
+
+            return isContinue;
         }
 
         static void ShowMoney()
@@ -152,73 +220,119 @@ namespace VendingMachine
         {
             Console.WriteLine($"Refund Money : ${_totalAmount}");
             _totalAmount = 0;
-
         }
 
-        static void ChooseProduct(int page)
+        static void BuyProduct()
         {
-            var productOptions = _products.Select(x => $"Product({x.Key}) Price:{x.Price}")
-                .Skip((page - 1) * 5).Take(5);
-            Console.WriteLine($"Buy : {string.Join(" , ", productOptions)}. Or Choose (A)Abandon choose product");
-            Console.WriteLine($"Please enten number: ");
+            var userInput = string.Empty;
+            var totalShelf = _shelfs.Count - 1;
+            var selectedShelf = new ShelfModel(); ;
+            var selectedProduct = new ProductModel();
 
-            var userInput = Console.ReadLine();
-            var selectedProduct = _products.FirstOrDefault(x => userInput == x.Key.ToString());
-            if (userInput.Equals("A", StringComparison.OrdinalIgnoreCase) ||
-                userInput.Equals("Abandon", StringComparison.OrdinalIgnoreCase))
+            Console.WriteLine($"Choose Product Shelf Number (0~{totalShelf})");
+            userInput = Console.ReadLine();
+            if (!int.TryParse(userInput, out var tmp1) ||
+                tmp1 < 0 ||
+                tmp1 > totalShelf)
             {
-                Console.WriteLine("Leave choose product");
-            }
-            else if (selectedProduct.Key == 0)
-            {
-                Console.WriteLine("Can't find product");
+                Console.WriteLine($"Not Found Shelf.");
+                return;
             }
             else
             {
-                var productPrice = selectedProduct.Price;
-                var temperature = "Hot";
-                var burdens = new List<string>();
-                burdens.Add(GetAddBurden("ice"));
-                burdens.Add(GetAddBurden("lemon"));
+                selectedShelf = _shelfs.First(x => x.Idx == tmp1);
+            }
 
-                Console.WriteLine("Choose Product is (C)Cold +$5 or (H)Hot ?");
-                userInput = Console.ReadLine();
-                if (userInput.Equals("C", StringComparison.OrdinalIgnoreCase) ||
-                userInput.Equals("Cold", StringComparison.OrdinalIgnoreCase))
-                {
-                    productPrice += 5;
-                    temperature = "Cold";
-                }
+            var (requireds, optionals) = GetShelfOptions(selectedShelf.Idx);
+            var products = _products.Where(x => x.ShelfIdx == selectedShelf.Idx);
+            var productOptions = products.Select(x => $"({x.LocationIdx}). ${x.Price}");
+            Console.WriteLine($"Choose Product Number {string.Join(", ", products.Select(x => x.LocationIdx))}");
+            userInput = Console.ReadLine();
+            if (!int.TryParse(userInput, out var tmp2) ||
+                tmp2 < 0 ||
+                tmp2 > products.Max(x => x.LocationIdx))
+            {
+                Console.WriteLine($"Not Found Product.");
+                return;
+            }
+            else
+            {
+                selectedProduct = products.First(x => x.LocationIdx == tmp2);
+            }
 
-                if (_totalAmount < productPrice)
+            var userChooseResult = ChooseOptionalMixture(optionals);
+            if (_totalAmount < selectedProduct.Price)
+            {
+                Console.WriteLine($"Shelf {selectedShelf.Idx}, Product ({selectedProduct.LocationIdx}) price : ${selectedProduct.Price}, but there is only : ${_totalAmount}");
+                return;
+            }
+
+            _totalAmount -= selectedProduct.Price;
+            Console.WriteLine($"Buy Shelf {selectedShelf.Idx}, location {selectedProduct.LocationIdx} product, with {string.Join(",", requireds.Select(x => x.Name))} and {string.Join(",", userChooseResult.Select(x => x.Name))}");
+        }
+
+        static (List<MixtureModel> required, List<MixtureModel> optional) GetShelfOptions(int shlfIdx)
+        {
+            var selected = _shelfs.First(x => x.Idx == shlfIdx);
+            return (selected.Required, selected.Optional);
+        }
+
+        static List<MixtureModel> ChooseOptionalMixture(List<MixtureModel> options)
+        {
+            if (options == null)
+            {
+                return new List<MixtureModel>();
+            }
+
+            var selectedResult = new List<MixtureModel>();
+            foreach (var item in options)
+            {
+                Console.WriteLine($"Do you Add {item.Name} (Y/N)?");
+                var userInput = Console.ReadLine();
+                if (userInput.Equals("y", StringComparison.OrdinalIgnoreCase))
                 {
-                    Console.WriteLine($"Product ({selectedProduct.Key}) {temperature} price : ${productPrice}, but there is only : ${_totalAmount}");
-                }
-                else
-                {
-                    var addBurden = burdens.Any() ? $"Added {string.Join(", ", burdens)}" : string.Empty;
-                    Console.WriteLine($"Got Product ({selectedProduct.Key}) {temperature} {addBurden}");
-                    _totalAmount -= productPrice;
+                    selectedResult.Add(item);
                 }
             }
+
+            return selectedResult;
         }
 
-        static string GetAddBurden(string burden)
-        {
-            Console.WriteLine($"Add {burden} (Y/N) ?");
-            var userInput = Console.ReadLine();
-            return userInput.Equals("Y", StringComparison.OrdinalIgnoreCase) ?
-                burden :
-                string.Empty;
-        }
     }
 
-    public class MyProduct
+    public class ShelfModel
     {
-        public int Location { get; set; }
+        public int Idx { get; set; }
 
-        public int Key { get; set; }
+        public List<MixtureModel> Optional { get; set; }
+
+        public List<MixtureModel> Required { get; set; }
+    }
+
+    public class MixtureModel
+    {
+        public int Idx { get; set; }
+
+        public Mixture Name { get; set; }
 
         public decimal Price { get; set; }
+    }
+
+    public class ProductModel
+    {
+        public int ShelfIdx { get; set; }
+
+        public int LocationIdx { get; set; }
+
+        public decimal Price { get; set; }
+    }
+
+    public enum Mixture
+    {
+        Ice,
+        Milk,
+        Lemon,
+        Hot,
+        Cold,
     }
 }
